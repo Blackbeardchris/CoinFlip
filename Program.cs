8dd35fd8e9991ba5624780c9141da541bfefe054
Christopher Landry <christopherl10@learn.cssd.ab.ca>
2018-09-24 14:18:30 -0600

    ok lol

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace coinflip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int coin;
+            string userGuess;
+            Random rng = new Random();
+
+            //Query the user
+            Console.Write("Enter your guess Heads or Tails (H or T)");
+            userGuess = Console.ReadLine();
+
+            //get a random number
+            coin = rng.Next(0, 2);
+
+            if (coin == 0 && userGuess == "T"){
+                Console.WriteLine("the coin flip was Tails, you win!");
+            }
+            else if (coin == 1 && userGuess == "H"){
+                Console.WriteLine("The coin flip was Heads");
+            }
+            else {
+                if (coin == 0) {
+                    Console.WriteLine("The coin flip was tails, you lose");
+                }
+                else {
+                    Console.WriteLine("The coin flip was Heads, you lose!");
+                }
+            }
+            Console.ReadLine();
+
         }
     }
 }
